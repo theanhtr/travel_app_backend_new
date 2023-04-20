894b29733b155eb4d92c3ed5959bf10dae58ffe7
Trần Thế Anh <trtheanh.work@gmail.com>
2023-04-20 23:06:09 +0700

    create login with social

--- app/Http/Controllers/Auth/AuthenticationController.php
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Helper\GetRoleIdHelper;
 use App\Http\Controllers\Controller;
 use App\Models\Authentication;
 use App\Http\Requests\UpdatePasswordRequest;
 use App\Http\Requests\LoginAuthenticationRequest;
+use App\Http\Requests\LoginAccessTokenRequest;
 use App\Http\Requests\LogoutAuthenticationRequest;
 use App\Models\Role;
 use App\Models\User;
@@ -13,12 +15,13 @@ use App\Traits\HttpResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use GuzzleHttp\Client;
+use Illuminate\Support\Str;
 
 class AuthenticationController extends Controller
 {
     use HttpResponse;
-    public function login(LoginAuthenticationRequest $request)
-    {
+    public function login(LoginAuthenticationRequest $request) {
         $login = $request->only("email", "password");
         
         if(!Auth::attempt($login)) {
@@ -85,5 +88,73 @@ class AuthenticationController extends Controller
         });
 
         return $this->success("Password updated", "", 200);
+    }
+
+    public function loginGoogle(LoginAccessTokenRequest $request) {
+        try {
+            $client = new Client();
+            $access_token = $request -> access_token;
+            $checkToken = $client->get("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=$access_token");
+            $responseGoogle = json_decode($checkToken->getBody()->getContents(), true);
+            
+            return $this->checkUserByEmail($responseGoogle);
+        } catch (\Exception $e) {
+            return $this->failure($e->getMessage());
+        }
+    }
+
+    public function loginFacebook(LoginAccessTokenRequest $request) {
+        try {
+            $client = new Client();
+            $access_token = $request -> access_token;
+            $checkToken = $client->get("https://graph.facebook.com/v3.1/me?fields=id,name,email&access_token=$access_token");
+            $responseFacebook = json_decode($checkToken->getBody()->getContents(), true);
+
+            return $this->checkUserByEmail($responseFacebook);
+        } catch (\Exception $e) {
+            return $this->failure($e->getMessage());
+        }
+    }
+
+    public function checkUserByEmail($profile)
+    {
+        $user = User::where('email', $profile['email'])->first();
+        if (!$user) {
+            $user = User::create([
+                'email' => $profile['email'],
+                'password' => Hash::make(Str::random(8)),
+                'role_id' => GetRoleIdHelper::getCustomerRoleId(),
+                'email_verified_at' => now(),
+            ]);
+
+            $array_name = explode(" ", $profile['name']);
+
+            $first_name = $array_name[0];
+            $last_name = "";
+
+            for($i = 1; $i < count($array_name); $i++) {
+                $last_name .= $array_name[$i];
+                $last_name .= " ";
+            }
+
+            $user->information()->create([
+                'first_name' => $first_name,
+                'last_name' => $last_name,
+            ]);
+
+            $user -> avatar_id = 1;
+            $user -> save();
+        }
+        
+        $token = $user -> createToken($user->email);
+        $role_name = Role::find($user -> role_id)->name;
+
+        return $this->success('Login ok', [
+            "email" => $user->email,
+            "token" => $token -> accessToken,
+            "token_expires_at" => $token -> token -> expires_at, 
+            "role_name" => $role_name,
+            "role_id" => $user -> role_id
+        ]);
     }
 }
---
+++ app/Http/Requests/LoginAccessTokenRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Traits\HttpResponse;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class LoginAccessTokenRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules()
+    {
+        return [
+            'access_token' => 'required|string'
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Invalid input parameter structure',
+            'data'      => $validator->errors()
+        ], 500));
+    }  
+}
